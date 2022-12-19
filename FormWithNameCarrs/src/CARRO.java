8546e12ba306e355bb5f924df8c7c09a1474ec00
LeonardoHe4rt <LeonardoRafaeldev@gmail.com>
2022-12-19 15:00:00 -0300

    Add files via upload

---
+++ FormWithNameCarrs/src/CARRO.java
@@ -0,0 +1,14 @@
+
+public class CARRO {
+    
+    public String nome,cor;
+    public int ano;
+    
+    public CARRO(String nome, String cor, int ano){
+        this.nome = nome;
+        this.cor = cor;
+        this.ano = ano;
+    }
+    
+
+}
